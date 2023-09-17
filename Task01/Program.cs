497fd09c84f4e00537d66f94781120c486c22a52
AlexeyTen <aten9180929937@gmail.com>
2023-09-17 11:43:58 +0300

    add task1 and task2

---
+++ Task01/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число a");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число b");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+if (numB * numB == numA)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
