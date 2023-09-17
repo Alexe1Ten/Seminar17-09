f99fc229a7e603027548f7c11b5954e733b247d5
AlexeyTen <aten9180929937@gmail.com>
2023-09-17 16:28:29 +0300

    problem 4 solved

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -2,3 +2,27 @@
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
+try
+{
+    string result = "";
+    int max = 0;
+    for (int i = 1; i < 4; i++)
+    {
+        Console.WriteLine($"Введите число {i}: ");
+        int temp = Convert.ToInt32(Console.ReadLine());
+        if (i == 1)
+        {
+            max = temp;
+        }
+        if (temp > max)
+        {
+            max = temp;
+        }
+        result += temp.ToString() + " ";
+    }
+    Console.WriteLine($"{result}-> max = {max}");
+}
+catch (System.FormatException)
+{
+    Console.WriteLine("Ошибка ввода! Вы ввели не число.");
+}
