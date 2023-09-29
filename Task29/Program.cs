775b3c3c69f4c07f225bb2c9eaeffe137768849f
AlexeyTen <aten9180929937@gmail.com>
2023-09-29 20:18:22 +0300

    add Task29 and Task30

---
+++ Task29/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив
+// из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+int Input()
+{
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int [] Array(int count)
+{
+    int [] arr = new int[count];
+    for (int i = 0; i < count; i++)
+    {
+        Console.WriteLine($"Введите элемент {i+1}");
+        arr[i] = Input();
+    }
+    return arr;
+}
+
+Console.WriteLine("Сколько должно быть элементов в массиве?");
+int countElements = Input();
+
+int [] result = Array(countElements);
+Console.WriteLine($"[{string.Join(",", result)}]");
+
+
--- Task30/Program.cs
+++ Task30/Program.cs
@@ -3,10 +3,14 @@
 // нулями и единицами в случайном порядке.
 // [1,0,1,1,0,1,0,0]
 
-
-int Input()
+int InputInt()
 {
     return Convert.ToInt32(Console.ReadLine());
+}
+
+string InputStr()
+{
+    return Console.ReadLine();
 }
 
 int [] ArrayOfRandom(int from, int before, int quantity)
@@ -19,13 +23,41 @@ int [] ArrayOfRandom(int from, int before, int quantity)
     return randomArr;
 }
 
+int [] ArraySort(int [] arr)
+{
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        int maxIndex = i;
+        for (int j = i + 1; j < arr.Length; j++)
+        {
+            if (arr [j] > arr[maxIndex]) maxIndex = j;
+        }
+        int temp = arr[i];
+        arr[i] = arr[maxIndex];
+        arr[maxIndex] = temp;
+    }
+    return arr;
+}
+
+bool Choice(string choice)
+{
+    if (choice == "да" || choice == "yes" || choice == "y") return true;
+    else if (choice == "нет" || choice == "no" || choice == "n") return false;
+}
+
+
 Console.WriteLine("Сколько должно быть элементов в массиве?");
-int quan = Input();
+int quan = InputInt();
 Console.WriteLine("С какого числа начинаем?");
-int fr = Input();
+int fr = InputInt();
 Console.WriteLine("Каким числом заканчиваем?");
-int bef = Input();
+int bef = InputInt();
 
 int [] result = ArrayOfRandom(fr, bef, quan);
-Console.WriteLine($"[{string.Join(",", result)}]");
+Console.WriteLine($"[{string.Join(", ", result)}]");
+
+Console.WriteLine("Отсортировать получившийся массив? да/нет");
+string answer = InputStr();
+
+if (Choice(answer)) Console.WriteLine($"[{string.Join(", ", ArraySort(result))}]");
 
